74f196648f310b1366c6553ed77cb3bf81382a63
hjf <625598479@qq.com>
2022-08-23 17:28:16 +0800

    change monorepo

--- packages/smarty-ui-vite/docs/.vitepress/theme/index.ts
+++ packages/smarty-ui-vite/docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import Theme from 'vitepress/dist/client/theme-default'
-import SmartyUI from '../../../src/entry'
+import SmartyUI from '../../../src/entry';
 // 主题样式
 import 'vitepress-theme-demoblock/theme/styles/index.css'
 // 插件的组件，主要是demo组件
@@ -10,7 +10,7 @@ export default {
   ...Theme,
   enhanceApp({ app }) {
     app.use(SmartyUI)
-    app.component('Demo', Demo)
-    app.component('DemoBlock', DemoBlock)
+    // app.component('Demo', Demo)
+    // app.component('DemoBlock', DemoBlock)
   },
 }
